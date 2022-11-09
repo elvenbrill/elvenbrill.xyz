bbb9582a679e3c3a59fe481b035573db704d0de4
elvenbrill <elvenbrill@pm.me>
2022-11-09 14:41:55 +0200

    11ty

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,9 +2,18 @@ module.exports = {
   url: process.env.URL || "http://localhost:8080",
   lang: "bg",
   title: "Записките на Иван",
+  shortTitle: "EB",
   description:
     "Тези, които търсят истината, винаги трябва да бъдат предпочитани пред онези, които твърдят, че вече са я открили",
   themeColor: "#f4f4f5",
   themeColorDark: "#26272b",
+  accentColor: "#20e",
+  backgroundColor: "#fff",
+  author: {
+    name: "Elvenbrill",
+    url: "https://elvenbrill.xyz",
+    email: "elvenbrill@pm.me",
+    avatar: "/assets/icons/avatar.png",
+  },
   gitrepo: "https://github.com/elvenbrill/elvenbrill.xyz",
 };
